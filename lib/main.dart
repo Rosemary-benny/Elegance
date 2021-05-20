3c9660055fda882fce93ac7cf5ca9729f3462e49
neha771 <69786575+neha771@users.noreply.github.com>
2021-05-20 22:51:55 +0530

    home page 2

--- lib/main.dart
+++ lib/main.dart
@@ -93,23 +93,100 @@ class MyApp extends StatelessWidget {
                       physics: BouncingScrollPhysics(),
                       scrollDirection: Axis.horizontal,
                       child: Row(
+                  
                         children: [
+                          SizedBox(width: 20),
                           buildColumnWithRow("1","Harpa","29"),
                           buildColumnWithRow("2","Trends","23"),
                           buildColumnWithRow("3","Max","40"),
                           buildColumnWithRow("4","H&M","60"),
                         ],
                       ),
-                    )
-                  ],
-                ),
-              ),
-            )
-          ],
-        ),
+                    ),
+                    SizedBox(height: 20,),
+                    LineBar(),
+                    Padding(
+                      padding: const EdgeInsets.all(30.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+                      children: [
+                        Text(
+                          "Summer Wear",
+                          style: TextStyle(fontSize: 24),
+                        ),
+                        Text(
+                          "View All",
+                style: TextStyle(fontSize: 20, color: Colors.grey),
+                        ),
+                        ],
+                    ),
+                    ),
+                    SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: [
+              buildbottomContainer(),
+              buildbottomContainer(),
+              buildbottomContainer(),
+              buildbottomContainer(),
+            ],
+          ),
+        )
+      ],
+                 ),
+          ),
+          )
+        ],
+      ),
       ),
     );
   }
+
+    Container buildbottomContainer() {
+    return Container(
+      height: 150,
+      width: 230,
+      margin: EdgeInsets.only(left: 20),
+      child: Row(
+        children: [
+          ClipRRect(
+              borderRadius: BorderRadius.circular(30),
+              child: SizedBox(
+                  height: 120,
+                  width: 100,
+                  child: Image.asset(
+                    "assets/images/top4.jpg",
+                    fit: BoxFit.cover,
+                  ))),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("H&M Jacket"),
+                Text(
+                  "16 oz",
+                  style: TextStyle(color: Colors.grey, fontSize: 18),
+                ),
+                Spacer(),
+                Text(
+                  "\$ 28",
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w600,
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+                
+                    
 
   Padding buildColumnWithRow(String img,String title,String price) {
     return Padding(
@@ -185,4 +262,34 @@ class MyApp extends StatelessWidget {
                           }
                         
                         
+
+class LineBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // print(MediaQuery.of(context).size.height);
+    return Container(
+      height: 5,
+      width: MediaQuery.of(context).size.width,
+      margin: EdgeInsets.only(left: 40),
+      alignment: Alignment.centerLeft,
+      decoration: BoxDecoration(
+        color: Colors.grey[300],
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(10),
+          bottomLeft: Radius.circular(10),
+        ),
+      ),
+      child: Container(
+        height: 5,
+        width: 100,
+        decoration: BoxDecoration(
+          color: Colors.black,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(10),
+            bottomLeft: Radius.circular(10),
+          ),
+        ),
+      ),
+    );
+  }
 }
